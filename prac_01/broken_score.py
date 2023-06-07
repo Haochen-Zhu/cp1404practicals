9b7f7047127bda4dd7004847ea1e539368ed750e
zhuhaochen <zhuhaochen0@gmail.com>
2023-06-07 16:36:07 +1000

    add prac01 files

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -13,7 +13,7 @@ elif score < 0:
     print("Invalid score")
 elif score > 90:
     print("Excellent")
-elif score > 50:
+elif score >= 50:
     print("Passable")
 elif score < 50:
     print("Bad")
