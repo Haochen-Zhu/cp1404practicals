a98317e59d90d0ca9c02a6944e1e3a5fc89784bd
zhuhaochen <zhuhaochen0@gmail.com>
2023-06-11 23:17:44 +1000

    add prac01 files

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -1,25 +1,25 @@
-example.
+# example.
 for i in range(1, 21, 2):
     print(i, end=' ')
 print()
 
-a.
+# a.
 for i in range(0, 101, 10):
     print(i, end=' ')
 print()
 
-b.
+# b.
 for i in range(20, 0, -1):
     print(i, end=' ')
 print()
 
-c.
+# c.
 n = int(input("number of stars:"))
 for i in range(1, n+1, 1):
     print("*", end='')
 print()
 
-d.
+# d.
 n = int(input("number of rows:"))
 for i in range(n+1):
     for j in range(i):
