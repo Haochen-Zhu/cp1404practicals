83d71e396a76c354f19bed1d2d2c804b89ace805
zhuhaochen <zhuhaochen0@gmail.com>
2023-06-12 02:13:24 +1000

    refactored

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -7,15 +7,23 @@ password_len_min: int = 6
 def main():
     """change password to stars"""
     password = get_password()
-    password_len = int(len(password))
+    password_len = get_password_length(password)
     while password_len < password_len_min:
-        print(password_len < password_len_min)
         print("Password too short")
         password = get_password()
-        password_len = int(len(password))
+        password_len = get_password_length(password)
 
+    print_stars(password_len)
+
+
+def print_stars(password_len):
     for i in range(password_len):
         print("*", end='')
+
+
+def get_password_length(password):
+    password_len = int(len(password))
+    return password_len
 
 
 def get_password():
